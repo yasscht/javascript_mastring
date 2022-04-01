a6ecffe82ffc64b73a3fad4e1feeecdda0d31f79
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-01 21:02:58 +0000

    add the union and the intersection methods do the set

--- dataStructure/set.js
+++ dataStructure/set.js
@@ -9,7 +9,9 @@ class mySet {
     }
     return false;
   }
-
+  values() {
+    return this.values;
+  }
   //values : return the values of the set
   setValues() {
     return this.values.map((element) => element);
@@ -27,19 +29,53 @@ class mySet {
   //remove : remove an element from the set
   remove(element) {
     if (this.has(element)) {
-      this.values = this.values.filter((item) => item != element);
-      return this.values;
+      let index = this.values.indexOf(element);
+      this.values.splice(index, 1);
     }
     return false;
+  }
+  //size : return the size of the set
+  size() {
+    return this.values.length;
+  }
+  //union : return the union if two sets
+  union(newSet) {
+    const union = new mySet();
+    this.values.forEach(function (element) {
+      union.add(element);
+    });
+
+    newSet.values.forEach(function (element) {
+      union.add(element);
+    });
+
+    return union;
+  }
+  //intersection : this method returns the intersection of two sets
+
+  intersection(newSet) {
+    const intersection = new mySet();
+    this.values.forEach(function (element) {
+      if (newSet.has(element)) {
+        intersection.add(element);
+      }
+    });
+    return intersection;
   }
 }
 
 const set = new mySet();
+const newset = new mySet();
 console.log(set.add(1));
 console.log(set.add(2));
 console.log(set.add(2));
+console.log(newset.add(1));
+console.log(newset.add(2));
+console.log(newset.add(3));
 console.log(set.add("yassine"));
 console.log(set.setValues());
 console.log(set.remove(1));
 console.log("after");
 console.log(set.setValues());
+console.log("union", set.union(newset));
+console.log("intersec", set.intersection(newset));
